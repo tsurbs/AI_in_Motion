6dbb786e52bccba88cb162f38318f53a0b7771d9
Theo Urban <theo.s.urban@gmail.com>
2021-08-04 14:49:56 -0400

    testing openai speed

--- AIComplete.py
+++ AIComplete.py
@@ -24,7 +24,7 @@ if len(promptxt) > 2000:
 response = openai.Completion.create(
     engine="davinci",
     prompt=promptxt,
-    temperature=0.8,
+    temperature=0.6,
     max_tokens=150,
     top_p=1,
     frequency_penalty=0,
